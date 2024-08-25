015d14123375de9cc7d04917b123fff5761dfa12
Glenn Nelson <glenn@hexcoder.us>
2024-08-25 15:07:06 -0400

    Add Observer Counter for Server

--- server/server.js
+++ server/server.js
@@ -38,7 +38,67 @@ Counter.Polling.publish = async function(meteorContext, documentName, mongoCurso
 
 
 
-
 // Note: The original `cursor.observe(...)` style used in many Meteor 2.0 applications, i.e. `tmeasday:publish-counts`
 // See: https://github.com/percolatestudio/publish-counts/blob/3fec0655bdd1dcf59c97e2762a8520e884254eb8/server/publish-counts.js
 Counter.Observer = {};
+Counter.Observer.publish = async function(meteorContext, documentName, mongoCursor, options) {
+    const collectionName = options?.collectionName ?? 'reactive-counter';
+
+    let initializing = true;
+    let handle = null;
+    let count = 0;
+
+    let observer = {
+        added: function (document) {
+            count += 1;
+
+            if (!initializing)
+                meteorContext.changed(collectionName, documentName, { count: count });
+        },
+        removed: function (document) {
+            count -= 1;
+
+            if (!initializing)
+                meteorContext.changed(collectionName, documentName, { count: count });
+        },        
+    };
+
+    // Cursor.count is officially deprecated, so reuse Meteor's stored cursor options, like
+    // https://github.com/meteor/meteor/blob/release-3.0/packages/mongo/mongo_driver.js#L876,
+    // but we reuse the method countDocuments available here
+    // https://github.com/meteor/meteor/blob/release-3.0/packages/mongo/mongo_driver.js#L772
+    // given that it applies the internal methods replaceTypes and replaceMeteorAtomWithMongo
+    // to the `selector` and `options` arguments
+    const cursorDescription = mongoCursor._cursorDescription;
+
+    // Note: Unsure why in the original code there is a shadowed `var count` and then an assignment...
+    //	We eliminated the shadow `count` which results in an double-count scenario..
+    //	On initialization, we get flooded with `observer.added` calls which hydrates the count... 
+    //  This is possibly related to our elimination of the `options.nonReactive` ???
+    // count = await mongoCursor._mongo.countDocuments(collectionName, selector, options);
+    await mongoCursor._mongo.countDocuments(cursorDescription.collectionName, cursorDescription.selector, cursorDescription.options);
+
+    handle = await mongoCursor.observe(observer);
+
+    meteorContext.added(collectionName, documentName, { count: count });
+
+    meteorContext.ready();
+
+    meteorContext.onStop(function() {
+        if (handle) {
+            handle.stop();
+            handle = null;
+        }
+    });
+
+    initializing = false;
+
+    return {
+        stop: function() {
+            if (handle) {
+                handle.stop();
+                handle = null;
+            }
+        }
+    };
+};
